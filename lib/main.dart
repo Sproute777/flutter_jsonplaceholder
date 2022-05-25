1b54e3e9bfab82e734ebfe84e6aeac43cdc4777e
sproute <prestiz4321@gmail.com>
2022-05-25 14:31:54 +0300

    lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,44 @@
-// import 'dart:ui';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_jsonplaceholder/src/pages/photos_infinity/view/photo_infinity_page.dart';
 
 import 'bootstrap.dart';
 
 import 'src/data/local_datasource/local_datasource.dart';
-// import 'src/pages/home/view/home_page.dart';
-// import 'src/pages/profite/view/profite_page.dart';
+import 'src/pages/profile/view/profile_page.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final AppDatabase database = AppDatabase();
-  final UsersDao todosDao = UsersDao(database);
-  final user1 = User(
-      id: 4,
-      name: 'Some name Alex',
-      username: 'other username Example',
-      email: 'example@mail.ru',
-      phone: '84405903940',
-      website: 'www.website.com');
-  // final map = user1.toJson();
-  // print('user is map ${map.runtimeType} ${map.toString()}');
+//   WidgetsFlutterBinding.ensureInitialized();
+//   final AppDatabase database = AppDatabase();
+//   final UsersDao todosDao = UsersDao(database);
+//   bootstrap(todosApi: todosDao);
 
-  bootstrap(todosApi: todosDao);
-  // runApp(MaterialApp(
-  //     scrollBehavior: MaterialScrollBehavior()
-  //         .copyWith(dragDevices: {PointerDeviceKind.mouse}),
-  //     home: ProfilePage(
-  //         user: User(
-  //             id: 4,
-  //             name: 'Some name Alex',
-  //             username: 'other username Example',
-  //             email: 'example@mail.ru',
-  //             phone: '84405903940',
-  //             website: 'www.website.com'),
-  //         address: Address(
-  //             userId: 4,
-  //             street: 'street',
-  //             suite: 'suite',
-  //             city: 'city',
-  //             zipcode: '948540',
-  //             lat: '456',
-  //             lng: '455'),
-  //         company: Company(
-  //             userId: 4,
-  //             name: 'compnay name',
-  //             catchPhrase: 'catchPhrase',
-  //             bs: 'bs'))));
+  runApp(MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior()
+          .copyWith(dragDevices: {PointerDeviceKind.mouse}),
+      home: const PhotoInfinityPage()));
+  // home: ProfilePage(
+  //     user: User(
+  //         id: 4,
+  //         name: 'Some name Alex',
+  //         username: 'other username Example',
+  //         email: 'example@mail.ru',
+  //         phone: '84405903940',
+  //         website: 'www.website.com'),
+  //     address: Address(
+  //         userId: 4,
+  //         street: 'street',
+  //         suite: 'suite',
+  //         city: 'city',
+  //         zipcode: '948540',
+  //         lat: '456',
+  //         lng: '455'),
+  //     company: Company(
+  //         userId: 4,
+  //         name: 'compnay name',
+  //         catchPhrase: 'catchPhrase',
+  //         bs: 'bs'))
+  //         )
+  // );
 }
