f4c55218a0a7b2ab54e9eaaec88092108a3ae8c0
sproute <prestiz4321@gmail.com>
2022-05-28 17:32:59 +0300

    go_router bottom_bar

--- lib/src/common/widgets/profile_tabs_bar.dart
+++ lib/src/common/widgets/profile_tabs_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../features/albums/view/album_page.dart';
 import '../../features/posts/view/posts_page.dart';
@@ -7,15 +8,29 @@ import '../models/models.dart';
 
 class ProfileTabBar extends StatefulWidget {
   final ProfileUser user;
+  final String kind;
 
-  const ProfileTabBar({Key? key, required this.user}) : super(key: key);
+  const ProfileTabBar(this.kind, {Key? key, required this.user})
+      : super(key: key);
 
   @override
   ProfileTabBarState createState() => ProfileTabBarState();
 }
 
 class ProfileTabBarState extends State<ProfileTabBar> {
-  int _selectedIndex = 1;
+  late int _selectedIndex;
+
+  @override
+  void initState() {
+    _selectedIndex = widget.kind.toIndex();
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProfileTabBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _selectedIndex = widget.kind.toIndex();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +44,52 @@ class ProfileTabBarState extends State<ProfileTabBar> {
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.red,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'text'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'user'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.collections), label: 'text2'),
+              icon: Icon(Icons.collections), label: 'albums'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.collections), label: 'text3'),
+              icon: Icon(Icons.collections), label: 'posts'),
         ],
         onTap: (int index) {
           if (_selectedIndex != index) {
-            setState(() {
-              _selectedIndex = index;
-            });
+            _handleTabTapped(index);
           }
         },
       ),
     );
+  }
+
+  void _handleTabTapped(int index) {
+    switch (index) {
+      case 1:
+        context.go('/profile/albums',
+            extra: <String, Object>{'profileUser': widget.user});
+        break;
+      case 2:
+        context.go('/profile/posts',
+            extra: <String, Object>{'profileUser': widget.user});
+        break;
+      case 0:
+      default:
+        context.go('/profile/user',
+            extra: <String, Object>{'profileUser': widget.user});
+        break;
+    }
+  }
+}
+
+extension StringExt on String {
+  int toIndex() {
+    switch (this) {
+      case 'albums':
+        return 1;
+
+      case 'posts':
+        return 2;
+
+      case 'user':
+      default:
+        return 0;
+    }
   }
 }
